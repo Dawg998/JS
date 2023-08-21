6d3b6528a6c66c4871c31a9583ceab027071920a
Dawg998 <140166220+Dawg998@users.noreply.github.com>
2023-08-21 18:19:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function convert(){
-    let cm=Number(document.getElementById("inputf").value);
-    let inch= cm/2.54;
-    let result=document.getElementById("result");
-    result.innerHTML=inch.toPrecision(4)+" inch";
-}
